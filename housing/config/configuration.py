bfbf611b9ed94ff08030ce1409611a7e19b3d680
PranayJogale <pranaypjogale@gmail.com>
2022-07-04 22:38:58 +0530

    update

---
+++ housing/config/configuration.py
@@ -0,0 +1,32 @@
+from housing.entity.config_entity import DataIngestionConfig , DataValidationConfig, DataTransformationConfig, ModelEvaluationConfig, ModelPusherConfig, ModelTrainerConfig \
+    TrainingPipelineConfig
+    
+
+
+
+
+class Configuration:
+
+    def __init__(self) -> None:
+        pass
+
+    def get_data_ingestion_config(self):
+        pass
+
+    def get_data_validation_config(self):
+        pass
+
+    def get_data_transformation_config(self):
+        pass
+
+    def get_model_trainer_config(self):
+        pass
+
+    def get_model_evaluation_config(self):
+        pass
+
+    def get_model_pusher_cpnfig(self):
+        pass
+
+    def get_training_pipline_config(self):
+        pass
